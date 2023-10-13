422923f721562539bf3cdfd7c85183ae03e4a9dd
Caio <caiogl1998@gmail.com>
2023-10-13 10:41:05 -0300

    Payment, one-to-one association

--- src/main/java/com/educandoweb/course/entities/OrderItem.java
+++ src/main/java/com/educandoweb/course/entities/OrderItem.java
@@ -24,6 +24,7 @@ public class OrderItem implements Serializable{
 	}
 
 	public OrderItem(Order order, Product product, Integer quantity, Double price) {
+		super();
 		id.setOrder(order);
 		id.setProduct(product);
 		this.quantity = quantity;
--- src/main/java/com/educandoweb/course/entities/Payment.java
+++ src/main/java/com/educandoweb/course/entities/Payment.java
@@ -1,40 +1,39 @@
 package com.educandoweb.course.entities;
 
 import java.io.Serializable;
-import java.util.HashSet;
-import java.util.Set;
-
-import com.fasterxml.jackson.annotation.JsonIgnore;
+import java.time.Instant;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.ManyToMany;
+import jakarta.persistence.MapsId;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
-import jakarta.persistence.Transient;
+
 
 @Entity
-@Table(name = "tb_category")
-public class Category implements Serializable{
+@Table(name = "tb_user")
+public class Payment implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	private String name;
+	private Instant moment;
 	
-	@JsonIgnore
-	@ManyToMany(mappedBy = "categories")
-	private Set<Product> products = new HashSet<>();
+	@OneToOne
+	@MapsId
+	private Order order;
 	
-	public Category() {
+	public Payment() {
 	}
 
-	public Category(Long id, String name) {
+	public Payment(Long id, Instant moment, Order order) {
 		super();
 		this.id = id;
-		this.name = name;
+		this.moment = moment;
+		this.order = order;
 	}
 
 	public Long getId() {
@@ -45,16 +44,20 @@ public class Category implements Serializable{
 		this.id = id;
 	}
 
-	public String getName() {
-		return name;
+	public Instant getMoment() {
+		return moment;
 	}
 
-	public void setName(String name) {
-		this.name = name;
+	public void setMoment(Instant moment) {
+		this.moment = moment;
 	}
-	
-	public Set<Product> getProducts() {
-		return products;
+
+	public Order getOrder() {
+		return order;
+	}
+
+	public void setOrder(Order order) {
+		this.order = order;
 	}
 
 	@Override
@@ -73,7 +76,7 @@ public class Category implements Serializable{
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		Category other = (Category) obj;
+		Payment other = (Payment) obj;
 		if (id == null) {
 			if (other.id != null)
 				return false;
@@ -81,5 +84,4 @@ public class Category implements Serializable{
 			return false;
 		return true;
 	}
-
 }
